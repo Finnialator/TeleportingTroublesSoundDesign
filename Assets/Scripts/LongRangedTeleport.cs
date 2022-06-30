97ee21c97ed102781e00d5280e95758ffaea4c00
Finnialator <55963012+Finnialator@users.noreply.github.com>
2022-06-30 14:28:37 +1000

    Basic game
    Adding the base game to the Repository

---
+++ Assets/Scripts/LongRangedTeleport.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongRangedTeleport : MonoBehaviour
+{
+    public Transform playerPos;
+    public bool longOnCooldown;
+    private float baseCooldown;
+    public float longCooldown = 10f;
+    public KeyCode m_long;
+    void Start()
+    {
+        longOnCooldown = false;
+    }
+    void LongTeleport()
+    {
+        if (Input.GetKey(m_long) && longOnCooldown == false) // Activates Long-Range Teleportation
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Finding point to teleport to
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                playerPos.transform.position = hit.point; // Teleport
+                longOnCooldown = true;
+                baseCooldown = longCooldown; // Cooldown reset
+            }
+        }
+        else if (longOnCooldown == true) // Cooldown calculations
+        {
+            baseCooldown -= Time.deltaTime;
+            if (baseCooldown <= 0)
+            {
+                longOnCooldown = false;
+            }
+        }
+    }
+    void Update()
+    {
+        LongTeleport();   
+    }
+}
---
+++ Assets/Scripts/ShortRangedTeleport.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortRangedTeleport : MonoBehaviour
+{
+    public Transform playerPos;
+    public bool shortOnCooldown;
+    private float baseCooldown;
+    public float shortCooldown = 3f;
+    public KeyCode m_short;
+    public float shortTeleport = 7.5f;
+    void Start()
+    {
+        shortOnCooldown = false;
+    }
+    void Update()
+    {
+        ShortTeleport();
+    }
+    void ShortTeleport()
+    {
+        if (Input.GetKey(m_short) && shortOnCooldown == false) // Activates Short-Ranged Teleportation
+        {
+            playerPos.transform.position += playerPos.transform.forward * shortTeleport; // Teleport
+            shortOnCooldown = true;
+            baseCooldown = shortCooldown; // Cooldown reset
+        }
+        if (shortOnCooldown == true) // Cooldown calculations
+        {
+            baseCooldown -= Time.deltaTime;
+            if (baseCooldown <= 0)
+            {
+                shortOnCooldown = false;
+            }
+        }
+    }
+}
