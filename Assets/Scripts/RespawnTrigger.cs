65d878a892b95ac646bef4e2e9ac59f90b4110b9
beneng91 <103171571+beneng91@users.noreply.github.com>
2022-07-07 15:49:56 +1000

    Added Lose Sound

--- Assets/Scripts/RespawnTrigger.cs
+++ Assets/Scripts/RespawnTrigger.cs
@@ -7,13 +7,23 @@ public class RespawnTrigger : MonoBehaviour
 {
     private bool isTriggered = false;
     private float delay;
+    //Sound
+    public AudioClip[] sounds;
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             isTriggered = true;
-            delay = 3f;
+            delay = 2f;
             // Play audio for Losing here
+            source.clip = sounds[0];
+            source.Play();
         }
     }
     private void Update()
